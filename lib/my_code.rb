0e255ed51fdd725dc493ea56db129fb838d9df6c
Adam  Shappy <ajs11300@gmail.com>
2019-12-25 22:12:54 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
 # Your Code Here
+def map (array)
+  new_array = []
+  i = 0 
+  while i < array.length do 
+    altered_integer = yield(array[i])
+    new_array << altered_integer
+    i += 1 
+  end 
+  new_array
+end 
+
+
+def reduce(array, optional = nil)
+  if optional
+    starting_point = optional
+    i = 0 
+  else
+    starting_point = array[0]
+    i = 1 
+  end 
+  while i < array.length do 
+    starting_point = yield(starting_point, array[i])
+    i += 1 
+  end 
+  starting_point
+end 
