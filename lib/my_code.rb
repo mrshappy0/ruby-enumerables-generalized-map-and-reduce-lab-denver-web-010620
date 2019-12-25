e5da3d3cc42732c79350612f978ff6b75892467f
Adam  Shappy <ajs11300@gmail.com>
2019-12-25 21:54:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,8 @@ def map (array)
   new_array = []
   i = 0 
   while i < array.length do 
-    yield(array[i])
+    altered_integer = yield(array[i])
+    new_array << altered_integer
   end 
   new_array
 end 
